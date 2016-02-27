3dfe891e3c56d56725e34bc5ead966e6b8bb32be
discordgr <Razer_greece@yahoo.gr>
2016-02-27 19:58:54 +0200

    Algorithm 0.3

--- Algorithm.py
+++ Algorithm.py
@@ -18,36 +18,36 @@ General =       {
 	 'Travel' : None,
 	 'Social Media' : None,
 	 'House Activities' : None,
-         'Exit' : None
+         'Escape' : None
                  }
 Menu = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 Interests = [] 
-
+temp = []
+temp1 = []
 
 i=1
-for k in sorted(General):
+for k in General:
     print (repr(i) + "." + repr(k))
     i = i + 1
 
+for key in General:
+    temp.append(key)
+
+print(temp)
+    
 k=0
 z=-1
 flag1=False
 while (k<20 and flag1==False):
     flag = False
     answer = input("Choose your interests : ")
-    for i in General.items():
+    while ((i<len(temp)) and (flag1==False)):
         z = z + 1
-        if (answer.lower()=='Exit'):
+        if (answer.lower()=='Escape'):
             flag1=True
-        if (answer.lower()==i):
+        if (answer.lower()==temp[i]):
             Interests[k] = i
             k = k + 1
-            General[i] = True
-        else:
-            if ((flag==False) and (z==19)):
-                print("Possibly wrong answer")
-                print("Try again")
-                flag=True
     k = k + 1
 
 k=0
